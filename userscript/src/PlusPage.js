22c7ef8d0709cd1ab2b2571da24ba4bf6eb8d2d6
Hellscap3d <72767525+Hellscap3d@users.noreply.github.com>
2023-10-07 17:25:56 -0500

    removed code editor it made the site haul ass

--- userscript/src/PlusPage.js
+++ userscript/src/PlusPage.js
@@ -1,24 +1,15 @@
 import React from "react";
 import "./App.css";
 
-import CodeEditor from '@uiw/react-textarea-code-editor';
-
 function PlusPage() {
-    function handleCSSChange(reactEvent) {
-        localStorage.setItem("customcss", reactEvent.target.value);
-    }
+    // function handleCSSChange(reactEvent) {
+    //     localStorage.setItem("customcss", reactEvent.target.value);
+    // }
     return (
         <div className="App">
             <header className="App-header">
                 <h1>SYNTAX+</h1>
-                <p>Custom CSS</p>
-                <CodeEditor
-                    value={localStorage.getItem("customcss")}
-                    onChange={handleCSSChange}
-                    language="css"
-                    placeholder="/* Syntax+ Custom CSS */"
-                />
-
+                <p>Custom CSS Disabled, it was too slow.</p>
             </header>
         </div>
     );
